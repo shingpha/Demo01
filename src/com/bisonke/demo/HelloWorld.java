38f6b1375f1726f67db405ddba016c23fb9b7a3a
shingpha <h-shingpha@163.com>
2021-06-23 13:01:00 +0800

    Initial commit

---
+++ src/com/bisonke/demo/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.bisonke.demo;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld.");
+        System.out.println("HelloWorld.");
+    }
+
+}
